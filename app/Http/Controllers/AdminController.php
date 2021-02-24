f6ae734bd55685ee90989e0e4a08e5c41974de76
Isaac Swift <ijs@swiftgang.net>
2021-02-24 15:51:35 -0600

    Seeder and factories

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use App\Models\Scout;
+use App\Models\Preference;
 
 class AdminController extends Controller
 {
 
     public function import_data() {
-        
+        $inputFileName = __DIR__ . '/sampleData/example1.xls';//todo
+        $spreadsheet = IOFactory::load($inputFileName);
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        $sheetData = array_slice($sheetData, 3);
+        foreach($sheetData as $row){
+            if($scoutExists)
+            
+            //create preference 
+            preg_match('/(.*) \((\d*)(?:st|nd|rd|th) Pref\)/', $row['A'],  $regex_result);
+            $preference->name = $regex_result[1];
+            $preference->rank = $regex_result[2];
+        }
+        var_dump($sheetData);
     }
 
     /* plan_week */
     public function plan_week() {
         $scouts = \App\Models\Scout::order_by_desc('age', 'rank');
-        while $still_filling {
+        while($still_filling){
             foreach($scouts as $scouts) {
                 // Try to insert scout into first preference
                 // if that's full, try second, and so on.
--- database/seeders/ProgramSeeder.php
+++ database/seeders/ProgramSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 
 class ProgramSeeder extends Seeder
 {
@@ -13,6 +16,65 @@ class ProgramSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('programs')->delete();
+        DB::table('programs')->insert([
+            [
+            'id'=> 1,
+            'name'=> 'Huck Finn',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 2,
+            'name'=> 'Tree House',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 3,
+            'name'=> 'Fishing Outpost',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 4,
+            'name'=> 'Adventure Cove',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 5,
+            'name'=> 'ITSO',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 6,
+            'name'=> 'Five Stand',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 7,
+            'name'=> 'Bike Trek',
+            'max_participants'=> 12,
+            'min_scout_age'=> 13,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ],  [
+            'id'=> 8,
+            'name'=> 'Bike Outpost',
+            'max_participants'=> 10,
+            'min_scout_age'=> 12,
+            'created_at'    => Carbon::now(),
+            'updated_at'    => Carbon::now()
+        ]
+        ]);
     }
 }
