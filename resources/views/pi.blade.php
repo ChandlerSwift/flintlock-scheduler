dfd20d5c2f96652f37b581307eda2e047bec10e6
Isaac Swift <ijs@swiftgang.net>
2021-07-30 12:47:48 -0500

    pi page looks

--- resources/views/pi.blade.php
+++ resources/views/pi.blade.php
@@ -10,6 +10,7 @@
         border: solid 1px;
         margin: auto;
         border-color: white;
+        width: 100%;
     }
     th{
         background-color: #333;
@@ -19,11 +20,10 @@
     }
     td{
         border: solid 1px;
-        border-color: #c9c9c9;
-        padding: 5px;
+        border-color: white;
     }
     #small table{
-        border-color: #c9c9c9;
+        border-color: #c9c9c9;  
         
     }
     #small th{
@@ -34,19 +34,38 @@
         color: white;
         padding-top: 12px;
         padding-bottom: 12px;
+
+        padding-left: 7px;
     }
     #small tr:nth-child(even){background-color: #f2f2f2;}
     #small td{
-        
+        padding: 5px
+    ;
     }
 </style>
 <table >
     @foreach($programs as $program)
     <tr>    
         @if ($program->name == 'International Target Sports Outpost')
-        <td style="vertical-align: top;font-weight: bold;" >I.T.S.O.</td>
+        <td style="vertical-align: top;font-weight: bold;" >
+        <table id="small">
+                    <tr id="small">
+                        <th id="small">
+                            <b>I.T.S.O.</b>
+                        </th>
+                    </tr>
+                </table>
+        </td>
         @else
-        <td style="vertical-align: top;font-weight: bold;" >{{ $program->name }}</td>
+        <td style="vertical-align: top;font-weight: bold;" >
+        <table id="small">
+                    <tr id="small">
+                        <th id="small">
+                            <b>{{$program->name}}</b>
+                        </th>
+                    </tr>
+                </table>
+        </td>
         @endif
         @foreach($program->sessions->sortBy('start_time') as $session)
             <td style="vertical-align: top;">
@@ -74,12 +93,26 @@
                     @endforeach
                     </table>
                 @else
-                    
+                <table id="small">
+                    <tr id="small">
+                        <th id="small">
+                            <b>No Session Scheduled</b>
+                        </th>
+                    </tr>
+                </table>
                 @endif
             </td>
             @if($loop->last){{-- add empty cells for remaining days, if any --}}
                 @for($i = $loop->iteration; $i < 5; $i++)
-                    <td style="vertical-align: top;">No session scheduled.</td>
+                    <td style="vertical-align: top;">
+                    <table id="small">
+                    <tr id="small">
+                        <th id="small">
+                            <b>No Session Scheduled</b>
+                        </th>
+                    </tr>
+                </table>
+                </td>
                 @endfor
             @endif
         @endforeach
