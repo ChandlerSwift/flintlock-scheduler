81581ba62d5d299bc7496c3d1445211488168e5c
Chandler Swift <chandler@chandlerswift.com>
2022-07-04 20:10:09 -0500

    Make rosters print with checkboxes

--- resources/views/print/rosters.blade.php
+++ resources/views/print/rosters.blade.php
@@ -17,7 +17,9 @@
 
         <ul>
         @foreach($session->scouts as $scout)
-            <li><a href="/scouts/{{$scout->id}}">{{ $scout->first_name }} {{ $scout->last_name }} ({{ $scout->gender }}, {{ $scout->age }}), {{ $scout->site }}, {{ $scout->unit }}@if(!$scout->meetsReqsFor($session->program)) (needs {{ implode(', ', $scout->missingReqsFor($session->program)->pluck('name')->all()) }}) @endif</a></li>
+            <li style="list-style-type: '\2610'; padding-left: 0.5em;">
+                <a href="/scouts/{{$scout->id}}">{{ $scout->first_name }} {{ $scout->last_name }} ({{ $scout->gender }}, {{ $scout->age }}), {{ $scout->site }}, {{ $scout->unit }}@if(!$scout->meetsReqsFor($session->program)) (needs {{ implode(', ', $scout->missingReqsFor($session->program)->pluck('name')->all()) }}) @endif</a>
+            </li>
         @endforeach
         </ul>
     @endif
