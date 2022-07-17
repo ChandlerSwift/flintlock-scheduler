3c7daaa8fce3dfcaa06acf49f1ab915b195ccb22
Chandler Swift <chandler@chandlerswift.com>
2022-07-17 15:48:33 -0500

    Add note if no weeks are set up yet.

--- resources/views/select_week.blade.php
+++ resources/views/select_week.blade.php
@@ -2,13 +2,20 @@
 @section('content')
 <h1>Pick a week!</h1>
 <ul>
-@foreach($weeks as $week)
+@forelse($weeks as $week)
 <li>
     <a href="/weeks/{{ $week->id }}">
         {{ $week->name }}
         ({{ $week->start_date->format('l, Y-m-d') }}&ndash;{{ $week->start_date->addDays(6)->format('l, Y-m-d') }})
     </a>
 </li>
-@endforeach
+@empty
+    There are currently no weeks set up.
+    @if(Auth::user()->admin)
+        Set one up in the <a href="/admin/weeks">Weeks editor</a>.
+    @else
+        Ask an administrator to set up your week for you.
+    @endif
+@endforelse
 </ul>
 @endsection
